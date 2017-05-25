e07647c2a75b7ce1248d0dcb08e3ea049a590701
ColinTree <502470184@qq.com>
2017-05-25 12:22:27 +0800

    Update phpclean.js

--- js/phpclean.js
+++ js/phpclean.js
@@ -3,10 +3,15 @@ $(document).ready(function(){
 		var code=$('textarea').val();
 		var len=code.length;
 		var replaceList=['(',')','{','}','<','>','=','!',',','.',';','&','|','+','-','*','/','\\','%',':','case ','?'];
-		replaceList.forEach(function(e){
-			code=code.replace(e+' ',e);
-			code=code.replace(' '+e,e);
-		});
+		while(true){
+			var code_=code;
+			replaceList.forEach(function(e){
+				code=code.replace(e+' ',e);
+				code=code.replace(' '+e,e);
+			});
+			if(code_==code)
+				break;
+		}
 		$('textarea').val(code);
 		var percent=(len==0 ? 0 : Math.round(code.length/len*10000)/100);
 		$('div#form-framework h4 span').text(''+len+' => '+code.length+' ('+percent+'%)');
